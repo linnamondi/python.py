be771f78032726ea23a058708efd1e4b48362e57
Linn Amondi <linnamondi@gmail.com>
2025-05-30 11:17:44 +0300

    added file dataType file

---
+++ dataType.py
@@ -0,0 +1,81 @@
+# What data type is the value 42?
+x=42
+print(type(x))
+
+# Check the type of 3.14.
+w=3.14
+print(type(w))
+
+# What data type is "Hello"?
+j="Hello"
+print(type(j))
+
+# . Create a variable with boolean value True.
+g=True
+print(type(g))
+
+# . What type will be the result of 5 / 2?
+results=5 / 2
+print(type(results))
+
+# Convert the integer 5 to a float.
+u=5
+print(float(u))
+
+# . Convert the string "100" to an integer.
+d="100"
+print(int(d))
+
+# Check if the type of None is NoneType.
+a=None
+print(type(a))
+
+# Q9. What is the type of a list like [1, 2, 3]?
+p=[1, 2, 3]
+print(type(p))
+
+# . What is the type of a tuple (1, 2, 3)?
+i=(1, 2, 3)
+print(type(i))
+
+# Q11. What data type is a dictionary like {"a": 1, "b": 2}?
+n={"a": 1, "b": 2}
+print(type(n))
+
+# . How to check if a variable x is of type integer?
+v=10
+print(isinstance(v,int))
+
+# What data type results from 3 + 4j?
+h=3+4j
+print(type(h))
+
+# Convert a boolean False to integer.
+s=False
+print(int(s))
+
+b=True
+print(int(b))
+
+# Convert a float 2.718 to integer
+m= 2.718
+print(int(m))
+
+# Create a variable y with value 10 and then change it to a string.
+y=10
+y=str(y)
+print(type(y))
+
+
+# Q18. Check if 0 evaluates to False in a boolean context.
+c=0
+print(bool(c))
+
+# Check if an empty list [] is True or False in a boolean context.
+t=[]
+print(bool(t))
+
+
+# Q20. What is the type of b"byte string"?
+pp=b"byte string"
+print(type(pp))
